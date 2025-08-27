a730b3de8decdfb6cf09228684b3d79cca9236f9
jjack-1 <qazwsx0088@gmail.com>
2025-08-27 12:45:19 +0900

    게시글 렌더링 & 페이징 & 검색

--- src/pages/board/Home.js
+++ src/pages/board/Home.js
@@ -1,18 +1,57 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Form, FormControl, Pagination } from 'react-bootstrap';
 import BoardItem from '../../components/BoardItem';
 
 const Home = () => {
-  const [page, setPage] = useState(0);
   const [keyword, setKeyword] = useState('');
-  const [model, setModel] = useState({});
+  const [page, setPage] = useState(0);
+  const [model, setModel] = useState({
+    totalPage: undefined,
+    number: 0,
+    isFirst: true,
+    isLast: false,
+    boards: [],
+  });
 
-  async function apiHome() {}
+  useEffect(() => {
+    if (keyword) {
+      // 검색어 있을 때는 디바운스
+      const handler = setTimeout(() => {
+        apiHome();
+      }, 500);
 
-  function changeValue(e) {}
+      return () => clearTimeout(handler);
+    } else {
+      // 검색어 없으면 즉시 실행 (페이지 이동 같은 경우)
+      apiHome();
+    }
+  }, [page]);
+
+  async function apiHome() {
+    let response = await axios({
+      method: 'get',
+      url: `http://localhost:8080?page=${page}&keyword=${keyword}`,
+    });
+
+    setModel(response.data.body);
+  }
+
+  function prev() {
+    setPage(page - 1);
+  }
+  function next() {
+    setPage(page + 1);
+  }
+
+  function changeValue(e) {
+    setKeyword(e.target.value);
+    console.log(e.target.value);
+  }
 
   return (
     <div>
+      {keyword}
       <Form className='d-flex mb-4' onSubmit={''}>
         <FormControl
           type='search'
@@ -24,16 +63,23 @@ const Home = () => {
         />
       </Form>
 
-      <BoardItem key={1} id={1} title={'제목1'} page={0} />
+      {model.boards.map((board) => (
+        <BoardItem
+          key={board.id}
+          id={board.id}
+          title={board.title}
+          page={page}
+        />
+      ))}
 
       <br />
       <div className='d-flex justify-content-center'>
         <Pagination>
-          <Pagination.Item onClick={''} disabled={false}>
+          <Pagination.Item onClick={prev} disabled={model.isFirst}>
             Prev
           </Pagination.Item>
 
-          <Pagination.Item onClick={''} disabled={false}>
+          <Pagination.Item onClick={next} disabled={model.isLast}>
             Next
           </Pagination.Item>
         </Pagination>
